c4c4d5e664239cb045119d83ea474611e0cd02ae
anfernee84 <eduard.rogulin@outlook.com>
2022-08-24 18:02:14 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,11 +2,14 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
-///////////////////////////////////////////////////////// gallery creation /////////////////////////////
-const imageCard = document.querySelector(".gallery");
-const imageGallery = fnCreateGallery(galleryItems);
-function fnCreateGallery(obj) {
-  return obj
+///////////////////////////////////////////////////////// gallery creation ///////////////////////////
+const rootGallery = document.querySelector(".gallery");
+const imageGallery = CreateGallery(galleryItems);
+rootGallery.insertAdjacentHTML("afterbegin", imageGallery);
+rootGallery.addEventListener("click", handleClickOnImage);
+
+function CreateGallery(arrOfObj) {
+  return arrOfObj
     .map((elem) => {
       const { preview, original, description } = elem;
       return `
@@ -20,29 +23,24 @@ function fnCreateGallery(obj) {
     })
     .join("");
 }
-imageCard.insertAdjacentHTML("afterbegin", imageGallery);
-///////////////////////////////////////////////////////// click on image handler //////////////////////////////////
-imageCard.addEventListener("click", clickOnImage);
-
-function clickOnImage(evt) {
+///////////////////////////////////////////////////////// click on image handler //////////////////////
+function handleClickOnImage(evt) {
   evt.preventDefault();
   if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
   const instance = basicLightbox.create(
-    `<div class="modal">
-        <img src="${evt.target.dataset.source}", width = "640", height = "480">
-    </div>`
+    `<img src="${evt.target.dataset.source}", width = "640", height = "480"></div>`
   );
   instance.show();
-  ////////////////////////////////////////////////////// close on Escape v1//////////////////////////////////////////
+  ////////////////////////////////////////////////////// close on Escape v1////////////////////////////
   //   document.onkeydown = function (evt) {
   //     if (evt.key == "Escape" || evt.key == "Esc" || evt.keyCode == 27) {
-  //       evt.preventDefault();
+  //      // evt.preventDefault();
   //       instance.close();
   //     }
   //   };
-  ////////////////////////////////////////////////////// close on Escape v2////////////////////////////////////////
+  ////////////////////////////////////////////////////// close on Escape v2////////////////////////////
   window.addEventListener("keydown", onEscape);
   function onEscape(evt) {
     const ESC_KEY_CODE = "Escape";
